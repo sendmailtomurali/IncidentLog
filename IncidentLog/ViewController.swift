09ab6e487011ee8bbc74e0ac1e12b398ca880c2b
Kalaiselvi Krishnan <kalaiselvikrishnan@kavitas-macbook-pro.local>
2018-11-27 08:20:12 -0500

    Notification added and tested for 1 sec delay with sound and no badges

--- IncidentLog/ViewController.swift
+++ IncidentLog/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import UserNotifications
 
 class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
     
@@ -19,6 +20,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Krishnan:Notification Parameters
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, Error in})
+        
         //Krishnan: Location Update settings
         self.locationManager.delegate = self
         self.locationManager.requestAlwaysAuthorization()
@@ -33,12 +38,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
 
         for currentLocation in locations{
             let Date_time = DateFormatter.localizedString(from: NSDate() as Date,dateStyle:DateFormatter.Style.short,timeStyle:DateFormatter.Style.short)
-            print (Date_time)
             let Loc_Lat = String(format: "%.6f",currentLocation.coordinate.latitude)
             let Loc_Lon = String(format: "%.6f",currentLocation.coordinate.longitude)
             print("\(index): \(currentLocation)")
             let Msg = Date_time + ", " + Loc_Lat + ", " + Loc_Lon
             print (Msg)
+            Notif(msgbody: "Update Received")
             IncidentController.addIncident(newIncident: Msg)
             //Krishnan: This is needed for refresh
             tableView.reloadData()
@@ -67,13 +72,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = IncidentController.Incident[indexPath.row]
-        //Krishnan:This is needed for message to show in multiple lines
+        //Krishnan:This is needed for message to show data in multiple lines
         cell.textLabel?.numberOfLines = 0
         return cell
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        //Set the header text
+        //Krishnan:Set the header text
         return "Log Summary"
     }
 }
 
+func Notif(msgbody: String){
+    
+    //Krishnan: Set the notification content
+    let content = UNMutableNotificationContent()
+    content.title = "Incident Log"
+    content.body = msgbody
+    content.sound = UNNotificationSound.default
+
+    //Krishnan: Set the notification trigger
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    
+    //Krishnan: Configure the request - combination of content and trigger
+    let request = UNNotificationRequest(identifier: "Possible Incidents", content: content, trigger: trigger)
+    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+}
