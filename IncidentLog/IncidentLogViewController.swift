3710d6e83a96368eac2cde4acacfc060e0bd6837
Murali Krishnane <sendmailtomurali@gmail.com>
2018-12-10 20:48:01 -0500

    Added most part but for tableview

--- IncidentLog/IncidentLogViewController.swift
+++ IncidentLog/IncidentLogViewController.swift
@@ -16,7 +16,6 @@ class IncidentLogViewController: UIViewController {
     @IBOutlet weak var lbl_walking: UILabel!
     @IBOutlet weak var lbl_unknown: UILabel!
     
-    
     var MyDriving = String()
     var MyStationary = String()
     var MyWalking = String()
@@ -25,27 +24,12 @@ class IncidentLogViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        lbl_Driving.text = MyDriving
-        lbl_Stationary.text = MyStationary
-        lbl_walking.text = MyWalking
-        lbl_unknown.text = MyUnknown
+        
     }
     
-
-
     @IBAction func saveIncident(_ sender: Any) {
         IncidentController.addIncident(newIncident: textField.text!)
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- IncidentLog/ViewController.swift
+++ IncidentLog/ViewController.swift
@@ -12,7 +12,7 @@ import UserNotifications
 import CoreMotion
 import CallKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var Driving_Label: UILabel!
     @IBOutlet weak var Stationary_Label: UILabel!
@@ -25,10 +25,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     let locationManager:CLLocationManager = CLLocationManager()
     let motionActivityManager = CMMotionActivityManager()
     var Msg = "First"
-    var Drv = "Dummy"
-    var Stn = "New"
-    var Wlk = "New1"
-    var Unk = "Unknown"
     var Confid = "New"
     var callObs : CXCallObserver!
     var callObserver: CXCallObserver!
@@ -42,10 +38,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let IncidentLogViewController = segue.destination as! IncidentLogViewController
-        IncidentLogViewController.MyDriving = Drv
-        IncidentLogViewController.MyStationary = Stn
-        IncidentLogViewController.MyWalking = Wlk
-        IncidentLogViewController.MyUnknown = Unk
+        //pass data from here
     }
     
     override func viewDidLoad() {
@@ -68,14 +61,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         //starting call observer to check user is in call or not
         self.callObs = CXCallObserver()
         self.callObs.setDelegate(self as CXCallObserverDelegate, queue: nil)
-        print("monitoring calls")
         
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = NSTimeZone() as TimeZone
         if CMMotionActivityManager.isActivityAvailable() {
             motionActivityManager.startActivityUpdates(to: OperationQueue.main) { (motion) in
                 self.Driving_Label.text = (motion?.automotive)! ? "Driving" : "Not Driving"
-                self.Drv = (motion?.automotive)! ? "Yes Driving\n" : "Not Driving\n"
                 self.Cycling_Label.text = (motion?.cycling)! ? "Yes Cycling" : "Not Cycling"
                 self.Stationary_Label.text = (motion?.stationary)! ? "Yes stationary" : "Not stationary"
                 //self.Msg = (motion?.automotive)! ? "Yes Driving\n" : "Not Driving\n"
@@ -117,8 +108,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
                     Notif(msgbody: " Not Driving and No active Call")
                 }
             }
-            print (Msg)
-     //       sendtoserver(Lat: Loc_Lat, Lon: Loc_Lon, Message: Msg)
             IncidentController.addIncident(newIncident: Msg)
             //Krishnan: This is needed for refresh
            // tableView.reloadData()
@@ -134,29 +123,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
         super.didReceiveMemoryWarning()
         //Dispose of any resources that can be recreated
     }
-    /*
-    //Krishnan: TABLE VIEW SETUP
-    //Krishnan: Set the number of sections in the tableview as 1
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    //Krishnan: The number of rows within that one section is count of records
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return IncidentController.Incident.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = IncidentController.Incident[indexPath.row]
-        //Krishnan:This is needed for message to show data in multiple lines
-        cell.textLabel?.numberOfLines = 0
-        return cell
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        //Krishnan:Set the header text
-        return "Log Summary"
-    }*/
+
 }
 //Krishnan: Function writtent to notify user
 func Notif(msgbody: String){
@@ -197,46 +164,3 @@ extension ViewController: CXCallObserverDelegate {
         }
     }
 }
-/*
-//Krishnan:SEND DATA TO SERVER
-func sendtoserver(Lat:String,Lon:String,Message: String){
-    DispatchQueue.main.async(execute:{
-        let token = "etunEpk-06U:APA91bHiohffPemYEb6ILjSgdRTxdT9jPSZ3UMIOIeq_MqBQS8KberiqUGKyJaofhMqdvFkhN1lEwiUnkbJ5byikqJStZ9krhKWtd5xkGqJb4CyaSjawXlePKg_QAYeYpwcbr6MNooTK"
-        print(token)
-        let auth = "JWT \(token)"
-        let parameters = ["inc_lat": Lat,"inc_lng": Lon,"inc_speed":0.0,"inc_summary": Message,"inc_parent":1] as [String : Any]
-        print(parameters)
-        //for testing
-        // guard let url = URL (string: "http://ec2-54-191-172-248.us-west-2.compute.amazonaws.com/sandbox/v1/user/incident") else{ return}
-        //for production
-        guard let url = URL (string: "http://ec2-54-191-172-248.us-west-2.compute.amazonaws.com/api/v1/user/incident") else{ return}
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])else{ return}
-        request.httpBody = httpBody
-        request.addValue(auth, forHTTPHeaderField: "Authorization")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            print(request)
-            //ramya added 4-7-2018 for verify status code
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                
-            }
-            else{
-                if let data = data{
-                    do{//to handle crash
-                        print(auth)
-                    }catch{
-                        print(error)
-                    }
-                }
-            }
-            print(response)
-            }.resume()
-    })
-}//token
-
-*/
